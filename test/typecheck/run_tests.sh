41fca13fd14c0df4cd214e90bc8325ffd7f7b4d3
Yao Li <hnkfliyao@gmail.com>
2018-06-07 13:13:27 -0700

    Fix the path for cygwin.

--- test/typecheck/run_tests.sh
+++ test/typecheck/run_tests.sh
@@ -4,6 +4,14 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SAILDIR="$DIR/../.."
 
+UNAME="$(uname -s)"
+case "${UNAME}" in
+    CYGWIN*) DIR="$( cygpath -w $DIR)"
+             SAILDIR="$( cygpath -w $SAILDIR)"
+             ;;
+    *) ;;
+esac
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
